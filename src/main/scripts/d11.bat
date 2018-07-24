5b92f50891510f5de90472acf5eeb34b7bf479d3
dromelvan <dromelvan@aland.net>
2018-07-24 09:48:06 +0300

    Scripts.

--- src/main/scripts/d11.bat
+++ src/main/scripts/d11.bat
@@ -1,3 +1,17 @@
-cd ..
-java -jar bin/${project.build.finalName}.jar -c match_files
-cd bin
+ECHO OFF
+SETLOCAL ENABLEEXTENSIONS
+SET command=%1
+SET arg1=%2
+SET arg2=%3
+
+IF "%command%"=="matchday" (
+    IF DEFINED arg1 (
+	IF DEFINED arg2 (
+	    java -jar ${project.build.finalName}.jar -c %command% -match_day %arg1% -season %arg2%
+	) else {
+   	    java -jar ${project.build.finalName}.jar -c %command% -match_day %arg1%
+	}
+    ) else (
+        java -jar ${project.build.finalName}.jar -c %command%
+    )
+)
